33621c0fe6fc2e7469976c54d521fc11a155404f
mmackz <matthewmackenz@gmail.com>
2024-12-17 23:36:54 -0800

    feat: add coop mint cli deploy

--- src/config/accounts.ts
+++ src/config/accounts.ts
@@ -11,18 +11,14 @@ export type Account = {
 };
 
 export type Accounts = [Account];
+
+const key = process.env.ACCOUNT_1_KEY as Hex;
+const privateKey = privateKeyToAccount(key);
+
 export const accounts: Accounts = [
   {
-    account: "0x0000c1E5C9d12c8c52eb319AF11dA44bb84779d2",
-    key: process.env.ACCOUNT_1_KEY as Hex,
-  },
-].map((account) => {
-  return {
-    ...account,
-    privateKey: privateKeyToAccount(account.key as Hex),
-  } as {
-    account: Address;
-    key: Hex;
-    privateKey: PrivateKeyAccount;
-  };
-}) as Accounts;
+    account: privateKey.address as Address,
+    key,
+    privateKey,
+  }
+] as Accounts;
--- src/deploy.ts
+++ src/deploy.ts
@@ -1,21 +1,19 @@
-import { parseEther } from "viem";
 import {
   DeployableOptions,
   EventActionPayload,
   BoostCore,
   ManagedBudget,
+  Incentive,
 } from "@boostxyz/sdk";
 
 export async function deployBoost(
   core: BoostCore,
   budget: ManagedBudget,
   actionPayload: EventActionPayload,
+  incentive: Incentive,
   options?: DeployableOptions
 ) {
   console.log("deploying boost");
-  console.log(
-    await budget.available("0xf3B2d0E4f2d8F453DBCc278b10e88b20d7f19f8D")
-  );
 
   const action = core.EventAction(actionPayload);
 
@@ -24,16 +22,8 @@ export async function deployBoost(
     budget,
     action,
     allowList: core.OpenAllowList(),
-    incentives: [
-      core.ERC20PeggedIncentive({
-        asset: "0xf3B2d0E4f2d8F453DBCc278b10e88b20d7f19f8D",
-        peg: "0x0000000000000000000000000000000000000000",
-        reward: parseEther("0.5"),
-        limit: 2n,
-        manager: budget.assertValidAddress(),
-      }),
-    ],
-  });
+    incentives: [incentive],
+  }, options);
 
   console.log("boost deployed: ", boost.id);
 
